ac24956b63b110434a04b3338343313174aea562
SaverioFrancesco <mitramdhir@gmail.com>
2019-08-19 14:59:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-2xudga
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-2xudga)
---
+++ src/app/products.ts
@@ -0,0 +1,44 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 20000,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 1000,
+    description: ''
+  },  
+  {
+    name: 'Phone Me',
+    price: 4,
+    description: 'A great phone with one of the best cameras'
+  },  
+  {
+    name: 'Phone Me',
+    price: 800,
+    description: 'A great phone with one of the best cameras'
+  },  
+  {
+    name: 'Phone Me',
+    price: 8,
+    description: 'A great phone with one of the best cameras'
+  },  
+  {
+    name: 'Phone Me',
+    price: 800,
+    description: 'A great phone with one of the best cameras'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
